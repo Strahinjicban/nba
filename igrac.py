55c8aa0391d0fae0c5589cda01c284998d7edcb8
Strahinjicban <121462443+Strahinjicban@users.noreply.github.com>
2024-01-17 18:50:19 +0100

    Add files via upload

--- igrac.py
+++ igrac.py
@@ -1,1 +1,12 @@
-print("ime:Tracy Mcgrady")
+name     = "Tracy Mcgrady"
+birthday = "17.05.1979"
+born     = "Bartow, Florida"
+posision = "Shooting guard"
+carier   = "Time in NBA:1997-2013"
+print(name)
+print(birthday)
+print (born)
+print (posision)
+print ( carier)
+
+
